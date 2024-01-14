35d7db3f055b7e30c8d97556a785b147b04ac301
Gabriel <gabrielmarcelino099@gmail.com>
2024-01-14 14:35:50 -0300

    update mongoose connect

--- myApp.js
+++ myApp.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => console.log(err));
 
 let Person;
 
